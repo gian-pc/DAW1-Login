b6a67a03013cc1e554815fb2ee7dc34b9f1a8c58
gianpc <gpaucarcortez@gmail.com>
2025-01-18 15:33:50 -0500

    Se mejoro el diseño y se actualizo el controller

--- src/main/java/com/gianpc/waformulariosspring/controller/LoginController.java
+++ src/main/java/com/gianpc/waformulariosspring/controller/LoginController.java
@@ -5,11 +5,31 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+
 @Controller
 public class LoginController {
     @GetMapping("/login")
     public String login(Model model) { // enviar el objeto LoginModel desde el controlador hacia la vista
         model.addAttribute("loginModel", new LoginModel());
+        model.addAttribute("mostrarMensaje", false);
+        return "login";
+    }
+
+    @PostMapping("/login")
+    public String validarLogin(@ModelAttribute("loginModel") LoginModel loginModel,
+                               Model model){
+        if(loginModel.getUsuario().equals("gian") &&
+                loginModel.getPassword().equals("123")){
+            model.addAttribute("mensaje", "Bienvenido "
+                    + loginModel.getUsuario());
+            return "home";
+        }
+        model.addAttribute("mostrarMensaje", true);
+        model.addAttribute("mensaje",
+                "Usuario o contraseña incorrectos");
         return "login";
     }
 }
--- src/main/java/com/gianpc/waformulariosspring/model/LoginModel.java
+++ src/main/java/com/gianpc/waformulariosspring/model/LoginModel.java
@@ -9,4 +9,5 @@ import lombok.Setter;
 public class LoginModel {
     private String usuario;
     private String password;
+
 }
