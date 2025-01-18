47941986895b66bda07e1d4862056f4adb77e6bb
gianpc <gpaucarcortez@gmail.com>
2025-01-18 00:57:46 -0500

    modelo agregado
    Model permite la comunicación entre la vista y el controlador

--- src/main/java/com/gianpc/waformulariosspring/controller/LoginController.java
+++ src/main/java/com/gianpc/waformulariosspring/controller/LoginController.java
@@ -1,12 +1,15 @@
 package com.gianpc.waformulariosspring.controller;
 
+import com.gianpc.waformulariosspring.model.LoginModel;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class LoginController {
     @GetMapping("/login")
-    public String login() {
+    public String login(Model model) { // enviar el objeto LoginModel desde el controlador hacia la vista
+        model.addAttribute("loginModel", new LoginModel());
         return "login";
     }
 }
