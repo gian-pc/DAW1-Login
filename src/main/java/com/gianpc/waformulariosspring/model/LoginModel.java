d82365cf0a7257a31be6d9b6b9da188ab4a80665
gianpc <gpaucarcortez@gmail.com>
2025-01-17 20:40:31 -0500

    Agregando atributos al model

--- src/main/java/com/gianpc/waformulariosspring/model/LoginModel.java
+++ src/main/java/com/gianpc/waformulariosspring/model/LoginModel.java
@@ -1,4 +1,12 @@
 package com.gianpc.waformulariosspring.model;
 
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class LoginModel {
+    private String usuario;
+    private String password;
 }
