4516815cb32d76da890eeeb7af22d875582ccc9b
gianpc <gpaucarcortez@gmail.com>
2025-01-20 10:20:52 -0500

    Se agrego el ejercicio de IMC

---
+++ src/main/java/com/gianpc/waformulariosspring/model/ImcModel.java
@@ -0,0 +1,12 @@
+package com.gianpc.waformulariosspring.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ImcModel {
+
+    private double talla;
+    private double peso;
+}
